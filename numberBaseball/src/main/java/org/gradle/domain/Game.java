21041cf8fe75492d2d55715701474bb3fd55e9ea
sungjine456 <sungjine456@gmail.com>
2016-12-21 18:53:02 +0900

    2인용 게임 만듬

--- numberBaseball/src/main/java/org/gradle/domain/Game.java
+++ numberBaseball/src/main/java/org/gradle/domain/Game.java
@@ -9,11 +9,14 @@ public class Game {
 		
 		while(true){
 			boolean finish = false;
-			System.out.println("게임을 선택해 주세요.\n1. 1인용\n0. 게임 종료(1, 2를 제외한 모든 숫자)");
+			System.out.println("게임을 선택해 주세요.\n1. 1인용\n2. 2인용\n0. 게임 종료(1, 2를 제외한 모든 숫자)");
 			int i = changeStringIsNumber(sc, sc.next());
 			switch (i) {
 				case 1:
 					playingOnePerson(sc);
+					break;
+				case 2:
+					playingTwoPeople(sc);
 					break;
 				default:
 					finish = true;
@@ -22,6 +25,60 @@ public class Game {
 			if(finish){
 				break;
 			}
+		}
+	}
+	
+	private void playingTwoPeople(Scanner sc){
+		System.out.println("숫자의 수를 정해주세요");
+		int numberSize = changeStringIsNumber(sc, sc.next());
+		Member memeber1 = new Member(numberSize);
+		Member memeber2 = new Member(numberSize);
+		
+		int win = 0;
+		while(true){
+			System.out.println("1인 숫자를 입력해주세요. ex) 1234");
+			String[] numbersStr = sc.next().split("");
+			if("a".equals(numbersStr[0])){
+				System.out.println("memeber1 : " + memeber1.showAnswer(numberSize));
+				continue;
+			}
+			if(confirmInputIsNotNumber(numberSize, numbersStr)){
+				System.out.print("다시 ");
+			}
+			System.out.println(memeber1.confirmAnswer(numbersStr));
+			if(memeber1.confirmAnswer(numbersStr).equals(numberSize + "S 0F")){
+				System.out.println("1인 성공 !!");
+				win += 1;
+			}
+			
+			System.out.println("2인 숫자를 입력해주세요. ex) 1234");
+			numbersStr = sc.next().split("");
+			if("a".equals(numbersStr[0])){
+				System.out.println("memeber1 : " + memeber2.showAnswer(numberSize));
+				continue;
+			}
+			if(confirmInputIsNotNumber(numberSize, numbersStr)){
+				System.out.print("다시 ");
+			}
+			System.out.println(memeber2.confirmAnswer(numbersStr));
+			if(memeber2.confirmAnswer(numbersStr).equals(numberSize + "S 0F")){
+				System.out.println("2인 성공 !!");
+				win += 2;
+			}
+			if(win != 0){
+				break;
+			}
+		}
+		switch (win) {
+		case 1:
+			System.out.println("1인 승리!");
+			break;
+		case 2:
+			System.out.println("2인 승리!");
+			break;
+		default:
+			System.out.println("무승부");
+			break;
 		}
 	}
 	
