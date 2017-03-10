7494ff725309d4d2b604051a88d07b4c6c62dc3c
sungjine456 <sungjine456@gmail.com>
2017-03-10 21:16:51 +0900

    Referee 리팩토링 & 단위 테스트 추가

--- numberBaseball/src/main/java/org/gradle/domain/Referee.java
+++ numberBaseball/src/main/java/org/gradle/domain/Referee.java
@@ -4,16 +4,18 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 import org.gradle.common.VerdictEnum;
 
 public class Referee implements Player {
+	private final int NUMBERS_BEFORE_SELECTION = 10;
+	
 	private List<Integer> numberList = new LinkedList<>();
 	
 	public Referee(int numberSize){
-		makeNumberList(numberSize);
-		shuffle(numberSize);
+		setNumberList(numberSize);
 	}
 	
 	@Override
@@ -29,21 +31,25 @@ public class Referee implements Player {
 	@Override
 	public String showAnswer(){
 		StringBuilder sb = new StringBuilder();
+		
 		int size = numberList.size();
 		IntStream.range(0, size).forEach(i->sb.append(numberList.get(i)+" "));
+		
 		return sb.toString();
 	}
 	
-	private void makeNumberList(int numberSize){
-		IntStream.range(0, 10).forEach(i->numberList.add(i));
+	private void setNumberList(int numberSize){
+		IntStream.range(0, NUMBERS_BEFORE_SELECTION).forEach(i->numberList.add(i));
+		
+		shuffle(numberSize);
 	}
 	
 	private void shuffle(int numberSize){
-		int i = 10;
+		int i = NUMBERS_BEFORE_SELECTION;
+		Random random = new Random();
 		while(numberList.size() > numberSize){
-			int index = (int)(Math.random() * i);
+			int index = (int)(random.nextInt(i--));
 			numberList.remove(index);
-			i--;
 		}
 		do{
 			Collections.shuffle(numberList);
--- numberBaseball/src/test/java/org/gradle/domain/RefereeTest.java
+++ numberBaseball/src/test/java/org/gradle/domain/RefereeTest.java
@@ -4,7 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 import org.junit.Test;
 
@@ -18,7 +22,6 @@ public class RefereeTest {
 		field.setAccessible(true);
 		Referee referee = (Referee)reCon.newInstance(10);
 		field.set(referee, Arrays.asList(1,2,3));
-		assertEquals("1 2 3 ", referee.showAnswer());
 		
 		String[] s = {"1","2","3"};
 		assertEquals("3S 0B", referee.confirmAnswer(s));
@@ -26,5 +29,64 @@ public class RefereeTest {
 		assertEquals("0S 3B", referee.confirmAnswer(s1));
 		String[] s2 = {"4","2","1"};
 		assertEquals("1S 1B", referee.confirmAnswer(s2));
+	}
+	
+	@Test
+	public void showAnswerTest() throws Exception {
+		Class<Referee> clazz = Referee.class;
+		Constructor<?>[] cons = clazz.getConstructors();
+		Constructor<?> reCon = cons[0];
+		Field field = clazz.getDeclaredField("numberList");
+		field.setAccessible(true);
+		Referee referee = (Referee)reCon.newInstance(10);
+		field.set(referee, Arrays.asList(1,2,3));
+		
+		assertEquals("1 2 3 ", referee.showAnswer());
+	}
+	
+	@Test
+	public void shuffleTest() throws Exception {
+		List<Integer> numberList = new ArrayList<>();
+		IntStream.range(0, 10).forEach(i->numberList.add(i));
+		
+		Class<Referee> clazz = Referee.class;
+		Constructor<?>[] cons = clazz.getConstructors();
+		Constructor<?> reCon = cons[0];
+		Field field = clazz.getDeclaredField("numberList");
+		field.setAccessible(true);
+		Method method = clazz.getDeclaredMethod("shuffle", new Class[]{Integer.TYPE});
+		method.setAccessible(true);
+		Referee referee = (Referee)reCon.newInstance(10);
+		field.set(referee, numberList);
+		
+		assertEquals("0 1 2 3 4 5 6 7 8 9 ", referee.showAnswer());
+		method.invoke(referee, new Object[]{3});
+		
+		String result = referee.showAnswer();
+		assertEquals(6, result.length());
+		assertEquals(5, result.trim().length());
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void setNumberListTest() throws Exception {
+		Class<Referee> clazz = Referee.class;
+		Constructor<?>[] cons = clazz.getConstructors();
+		Constructor<?> reCon = cons[0];
+		Field field = clazz.getDeclaredField("numberList");
+		field.setAccessible(true);
+		Method shuffleMethod = clazz.getDeclaredMethod("shuffle", new Class[]{Integer.TYPE});
+		shuffleMethod.setAccessible(true);
+		Method setNumberListMethod = clazz.getDeclaredMethod("setNumberList", new Class[]{Integer.TYPE});
+		setNumberListMethod.setAccessible(true);
+		Referee referee = (Referee)reCon.newInstance(10);
+		
+		List<Integer> list = (List<Integer>) field.get(referee);
+		assertEquals(10, list.size());
+		shuffleMethod.invoke(referee, new Object[]{3});
+		setNumberListMethod.invoke(referee, new Object[]{3});
+		
+		list = (List<Integer>) field.get(referee);
+		assertEquals(3, list.size());
 	}
 }
