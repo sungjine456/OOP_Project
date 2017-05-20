9d22763397ef3af6109025a008d3a5fbdd2818fd
sungjine456 <sungjine456@gmail.com>
2017-05-20 14:41:46 +0900

    전화번호부 프로젝트 준비

---
+++ PhoneBook/src/main/java/org/gradle/PhoneBookApplication.java
@@ -0,0 +1,12 @@
+package org.gradle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PhoneBookApplication {
+	private static final Logger log = LoggerFactory.getLogger(PhoneBookApplication.class);
+	
+	public static void main(String[] args){
+		log.info("Application Start");
+	}
+}
