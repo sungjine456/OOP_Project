67d246fefc29812d77592691c48b119edfd8351d
sungjine456 <sungjine456@gmail.com>
2017-06-07 18:33:18 +0900

    group class에 hashCode & equals & toString 오버라이딩

--- PhoneBook/src/main/java/org/gradle/domain/Group.java
+++ PhoneBook/src/main/java/org/gradle/domain/Group.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Group {
 	private String groupName;
-	private List<Contcat> contcats;
+	private final List<Contcat> contcats;
 	
 	public Group(String groupName){
 		this.groupName = groupName;
@@ -38,5 +38,32 @@ public class Group {
 	}
 	public int contcatSize(){
 		return contcats.size();
+	}
+	
+	@Override
+	public int hashCode(){
+		int prime = 31;
+		int result = 1;
+		result = prime * result + groupName.hashCode();
+		return result;
+	}
+	@Override
+	public boolean equals(Object o){
+		if(o==this) return true;
+		if(!(o instanceof Group)) return false;
+		Group group = (Group)o;
+		List<Contcat> contcatList = group.getContcats();
+		if(contcatList.size()!=contcats.size()) return false;
+		int len = contcats.size();
+		for(int i = 0; i < len; i++){
+			if(!contcatList.get(i).equals(contcats.get(i))){
+				return false;
+			}
+		}
+		return group.getGroupName().equals(groupName);
+	}
+	@Override
+	public String toString(){
+		return "groupName : " + groupName + ", contcat size : " + contcats.size();
 	}
 }
