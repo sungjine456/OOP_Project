30a45bd18f3d9df933ea8165c68bae4917755687
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2017-12-16 20:35:30 +0900

    주사위 테스트 수정

--- booroomarble/src/test/scala/oop/scala/domain/dice/DiceTest.scala
+++ booroomarble/src/test/scala/oop/scala/domain/dice/DiceTest.scala
@@ -2,13 +2,13 @@ package oop.scala.domain.dice
 
 import org.scalatest.FlatSpec
 
-class DiceTest extends FlatSpec{
-  "The eyes of the die" should "have a value between 1 and 6" in {
+class DiceTest extends FlatSpec {
+  "The pips of the dice" should "have a value between 1 and 6" in {
     val dice = Dice
-    for(i <- 1 to 100) {
+    for (i <- 1 to 100) {
       val pip: Int = dice.cast
 
-      if(pip >= 7 || pip <= 0) {
+      if (pip >= 7 || pip <= 0) {
         println(pip)
         fail()
       }
