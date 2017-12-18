87f66e8bf5fff9fa60b305c0b420a743e8f63cc4
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2017-12-18 21:12:56 +0900

    돈을 모아놓고 관리하기 위한 지갑에 대한 정의

---
+++ booroomarble/src/main/scala/oop/scala/domain/money/Wallet.scala
@@ -0,0 +1,39 @@
+package oop.scala.domain.money
+
+class Wallet {
+  // 가지고 있는 총 금액
+  var maxMoney: Int = 0
+
+  // 각 지폐 별로 가지고 있는 갯수
+  private var thousandWon: Int = 0
+  private var fiveThousandWon: Int = 0
+  private var tenThousandWon: Int = 0
+  private var fiftyThousandWon: Int = 0
+  private var hundredThousandWon: Int = 0
+  private var fiveHundredThousandWon: Int = 0
+
+  // 현재 가지고 있는 지폐 중 제일 최고가인 지폐
+  var topBankNote: Money = {
+    null
+  }
+
+  /**
+   * 돈을 받았으면 거슬러 줘야한다.
+   * @param money 내가 받은 금액
+   * @return 거슬러 줘야하는 금액
+   */
+  def put(money: Int): Int = {
+    maxMoney = maxMoney + money
+    0
+  }
+
+  /**
+   * 돈을 줬으면 거슬러 받아야한다.
+   * @param money 내가 줘야할 금액
+   * @return 내가 주는 금액
+   */
+  def get(money: Int): Int = {
+    maxMoney = maxMoney - money
+    0
+  }
+}
