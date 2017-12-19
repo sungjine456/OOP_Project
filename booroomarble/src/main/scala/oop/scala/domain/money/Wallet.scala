26f0f7a1cba5e6e38366155410bd5d5fee6566aa
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2017-12-19 20:53:36 +0900

    지갑 수정

--- booroomarble/src/main/scala/oop/scala/domain/money/Wallet.scala
+++ booroomarble/src/main/scala/oop/scala/domain/money/Wallet.scala
@@ -5,12 +5,14 @@ class Wallet {
   var maxMoney: Int = 0
 
   // 각 지폐 별로 가지고 있는 갯수
-  private var thousandWon: Int = 0
-  private var fiveThousandWon: Int = 0
-  private var tenThousandWon: Int = 0
-  private var fiftyThousandWon: Int = 0
-  private var hundredThousandWon: Int = 0
-  private var fiveHundredThousandWon: Int = 0
+  private val wallet: Map[Money, Int] = Map (
+    ThousandWon -> 0,
+    FiveThousandWon -> 0,
+    TenThousandWon -> 0,
+    FiftyThousandWon -> 0,
+    HundredThousandWon -> 0,
+    FiveHundredThousandWon -> 0
+  )
 
   // 현재 가지고 있는 지폐 중 제일 최고가인 지폐
   var topBankNote: Money = {
@@ -19,21 +21,20 @@ class Wallet {
 
   /**
    * 돈을 받았으면 거슬러 줘야한다.
-   * @param money 내가 받은 금액
+   * @param wallet 내가 받은 금액
    * @return 거슬러 줘야하는 금액
    */
-  def put(money: Int): Int = {
-    maxMoney = maxMoney + money
+  def put(wallet: Wallet): Int = {
+
     0
   }
 
   /**
    * 돈을 줬으면 거슬러 받아야한다.
-   * @param money 내가 줘야할 금액
+   * @param wallet 내가 줘야할 금액
    * @return 내가 주는 금액
    */
-  def get(money: Int): Int = {
-    maxMoney = maxMoney - money
+  def get(wallet: Wallet): Int = {
     0
   }
 }
