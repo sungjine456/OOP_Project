be1e1347089ddaa83f65ebf02b0e9dce3899fc25
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2017-12-29 13:53:50 +0900

    사용자의 기능 추가

--- booroomarble/src/main/scala/oop/scala/domain/user/User.scala
+++ booroomarble/src/main/scala/oop/scala/domain/user/User.scala
@@ -1,5 +1,27 @@
 package oop.scala.domain.user
 
-class User {
+import oop.scala.domain.card.CountryCard
+import oop.scala.domain.money.{ MoneyBundle, Wallet }
+import scala.collection.mutable.Set
 
+class User {
+  private val wallet: Wallet = new Wallet
+  private val countryCard: Set[CountryCard] = Set.empty
+
+  def payMoney(money: Int): Option[MoneyBundle] = {
+    // 지불해야하는 금액을 받고 지갑에서 돈을 꺼내 준다.
+    None
+  }
+
+  def getMoney(money: MoneyBundle): Unit = {
+    wallet.put(money)
+  }
+
+  def containsCard(card: CountryCard): Boolean = {
+    countryCard.contains(card)
+  }
+
+  def getCard(card: CountryCard): Unit = {
+    countryCard add card
+  }
 }
