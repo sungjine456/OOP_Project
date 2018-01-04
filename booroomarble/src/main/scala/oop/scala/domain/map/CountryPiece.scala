4cc6a2d7235bf183780b0abc426d68d308fe2d3f
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-01-04 22:06:48 +0900

    나라가 있는 칸에 대한 정의

--- booroomarble/src/main/scala/oop/scala/domain/map/CountryPiece.scala
+++ booroomarble/src/main/scala/oop/scala/domain/map/CountryPiece.scala
@@ -1,5 +1,23 @@
 package oop.scala.domain.map
 
-class CountryPiece extends Piece {
+import oop.scala.domain.card.CountryCard
+import oop.scala.domain.user.User
 
+case class CountryPiece(card: CountryCard) extends Piece {
+  var owner: Option[User] = None
+
+  // TODO: 방법1. event와 비슷한 유형을 만들고 event를 반환하도록 하여 시스템에서 처리하도록 한다?
+  override def visit(user: User): Unit = {
+    owner match {
+      case Some(o) if o == user => // TODO: 사용자가 건물 짓기를 원한다면 건물을 지을 수 있도록 해야한다.
+      case Some(o) =>
+        user.payMoney(card.totalTollFee) match {
+          case Some(m) => o.receive(m)
+          case None => // TODO: 방문자는 파산했으며 파산에 대한 기능을 만들어야한다.
+        }
+      case None =>
+        user.addCard(card)
+        owner = Some(user)
+    }
+  }
 }
