b46078688bff04e4bb23f58523c00d3fc7e54129
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-02-26 22:56:35 +0900

    황금 카드 변경

--- booroomarble/src/main/scala/oop/scala/domain/map/GoldCardPiece.scala
+++ booroomarble/src/main/scala/oop/scala/domain/map/GoldCardPiece.scala
@@ -1,5 +1,10 @@
 package oop.scala.domain.map
 
-class GoldCardPiece extends Piece {
+import oop.scala.domain.card.GoldCard
+import oop.scala.domain.user.User
 
+case class GoldCardPiece(card: GoldCard) extends Piece {
+  override def visit(user: User): Unit = {
+    card.publish(user)
+  }
 }
