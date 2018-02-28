d56f237bf93ea64dd4cc2a7d7aeb5304acbab41e
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-02-28 23:06:42 +0900

    지도에 카드 놓기

--- booroomarble/src/main/scala/oop/scala/domain/map/Map.scala
+++ booroomarble/src/main/scala/oop/scala/domain/map/Map.scala
@@ -1,14 +1,28 @@
 package oop.scala.domain.map
 
+import oop.scala.domain.card.CardRepository
+
 object Map {
   // 시작 칸
   // 땅 칸
   // 황금 카드를 뽑을 수 있는 칸
   // 각각의 칸을 담고 있을 하나의 리스트를 가진다.
-  // TODO: 원형 연결 리스트로 변경해야한다.
-  val map: List[Piece] = Nil
+  // 리스트로 하되 리스트의 크기를 넘어가면 0으로 돌아가게...
+  private val map: Seq[Piece] = Seq(new StartPiece)
+  private val MAX_SIZE = 12
+
+  initialized()
 
   def piece(index: Int): Piece = {
     map(index)
+  }
+
+  def initialized(): Unit = {
+    for (i <- 0 until MAX_SIZE) {
+      map :+ CountryPiece(CardRepository.getCountryCard(i))
+      if (i != 0 && i % 3 == 0) {
+        map :+ GoldCardPiece(CardRepository.getGoldCard(i / 3 - 1))
+      }
+    }
   }
 }
