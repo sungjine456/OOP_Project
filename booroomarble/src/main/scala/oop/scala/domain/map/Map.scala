2784b5d4b52b4933063b063a77f66a3806f0eae3
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-03-01 20:21:57 +0900

    스칼라의 Naming Convention 에 따른 변수명 수정

--- booroomarble/src/main/scala/oop/scala/domain/map/Map.scala
+++ booroomarble/src/main/scala/oop/scala/domain/map/Map.scala
@@ -9,7 +9,7 @@ object Map {
   // 각각의 칸을 담고 있을 하나의 리스트를 가진다.
   // 리스트로 하되 리스트의 크기를 넘어가면 0으로 돌아가게...
   private val map: Seq[Piece] = Seq(new StartPiece)
-  private val MAX_SIZE = 12
+  private val MaxMapSize = 12
 
   initialized()
 
@@ -18,7 +18,7 @@ object Map {
   }
 
   def initialized(): Unit = {
-    for (i <- 0 until MAX_SIZE) {
+    for (i <- 0 until MaxMapSize) {
       map :+ CountryPiece(CardRepository.getCountryCard(i))
       if (i != 0 && i % 3 == 0) {
         map :+ GoldCardPiece(CardRepository.getGoldCard(i / 3 - 1))
