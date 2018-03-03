a23db1fbad24bb79ce6778ec2efaba8bfb14a437
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-03-03 12:20:06 +0900

    간단하게 주사위를 통해 게임이 동작할 수 있도록 수정

--- booroomarble/src/main/scala/oop/scala/domain/system/System.scala
+++ booroomarble/src/main/scala/oop/scala/domain/system/System.scala
@@ -1,5 +1,34 @@
 package oop.scala.domain.system
 
-class System {
+import oop.scala.domain.dice.Dice
+import oop.scala.domain.map.{ Map, Piece }
+import oop.scala.domain.user.User
 
+class System(userCount: Int) {
+  private val map = Map
+  private val users: Seq[User] = Nil
+  private var activeIndex = 0
+
+  initialized()
+
+  def initialized(): Unit = {
+    for (i <- 0 until userCount) {
+      users :+ User(s"$i 번 유저")
+    }
+  }
+
+  def throwDice: Unit = {
+    val cast: Int = Dice.cast
+
+    val user: User = users(activeIndex)
+    val piece: Piece = map.move(cast, user)
+
+    piece.visit(user)
+
+    changeActiveIndex
+  }
+
+  private def changeActiveIndex: Unit = {
+    activeIndex = if (activeIndex + 1 >= userCount) 0 else activeIndex + 1
+  }
 }
