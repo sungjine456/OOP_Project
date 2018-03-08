1390e38cfcbcd68621d76cd1ebeb5d2709b29c2c
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-03-08 23:26:40 +0900

    진 사용자는 시스템에서 지우도록 수정

--- booroomarble/src/main/scala/oop/scala/domain/system/System.scala
+++ booroomarble/src/main/scala/oop/scala/domain/system/System.scala
@@ -10,7 +10,7 @@ import scala.collection.mutable.Map
 
 class System(userCount: Int) {
   private val map = oop.scala.domain.map.Map
-  private val users: Seq[User] = Nil
+  private var users: Seq[User] = Nil
   private val usersPosition: Map[User, Int] = Map.empty
   private var activeIndex = 0
 
@@ -25,7 +25,7 @@ class System(userCount: Int) {
   @tailrec
   private def activeUser: User = {
     if(users(activeIndex).finishPlaying) {
-      changeActiveIndex
+      dropUser(activeIndex)
 
       activeUser
     } else users(activeIndex)
@@ -56,5 +56,11 @@ class System(userCount: Int) {
 
   private def changeActiveIndex: Unit = {
     activeIndex = if (activeIndex > userCount) 0 else activeIndex + 1
+  }
+
+  private def dropUser(index: Int) {
+    val user: User = users(index)
+    usersPosition - user
+    users = users.filter(u => user != u)
   }
 }
