249240e8210dc448de399aa33d8417236beac804
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-04-04 23:59:19 +0900

    땅에 건물을 지었을 때 땅과 건물에 대한 모든 가치를 합하여 알 수 있는 기능 추가

--- booroomarble/src/main/scala/oop/scala/domain/card/CountryCard.scala
+++ booroomarble/src/main/scala/oop/scala/domain/card/CountryCard.scala
@@ -9,6 +9,17 @@ case class CountryCard(name: String, certificatePrice: Int) extends Card {
 
   def totalTollFee: Int = edifice.tollFee
 
+  def totalValueOfEdifice: Int = {
+    def edificeValue(calEdifice: Edifice, price: Int): Int = {
+      calEdifice.subEdifice match {
+        case Some(subEdifice) => edificeValue(subEdifice, calEdifice.price + price)
+        case None => calEdifice.price + price
+      }
+    }
+
+    edificeValue(edifice, 0)
+  }
+
   // TODO: 가격보다 높은 돈을 넘겨받을 때 처리 방법 생각하기.
   // TODO: 가격을 Int 형으로 받는게 맞는건가?
   def edificeUpgrade(price: Int): Boolean = {
--- booroomarble/src/test/scala/oop/scala/domain/card/CountryCardTest.scala
+++ booroomarble/src/test/scala/oop/scala/domain/card/CountryCardTest.scala
@@ -8,5 +8,22 @@ class CountryCardTest extends FlatSpec {
 
     assert(country.totalTollFee === 300000)
   }
+
+  "totalValueOfEdifice" should "return total value of edifice" in {
+    val country = CountryCard("서울", 1000)
+
+    assert(country.totalValueOfEdifice === 1000)
+
+    assert(country.edificeUpgrade(2000))
+
+    assert(country.totalValueOfEdifice === 3000)
+
+    assert(country.edificeUpgrade(4000))
+
+    assert(country.totalValueOfEdifice === 7000)
+
+    assert(country.edificeUpgrade(8000))
+
+    assert(country.totalValueOfEdifice === 15000)
   }
 }
