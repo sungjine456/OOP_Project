6ee543629bae095dd3df1943134f540260e1b514
DESKTOP-CUU6PMC\sungjin <sungjine456@gmail.com>
2018-04-05 00:03:10 +0900

    테스트 명칭 및 테스트 내용 수정

--- booroomarble/src/test/scala/oop/scala/domain/card/CountryCardTest.scala
+++ booroomarble/src/test/scala/oop/scala/domain/card/CountryCardTest.scala
@@ -3,10 +3,22 @@ package oop.scala.domain.card
 import org.scalatest.FlatSpec
 
 class CountryCardTest extends FlatSpec {
-  "price()" should "have a different amount of money to pay for each edifice" in {
-    val country = CountryCard("서울", 100000)
+  "totalTollFee()" should "have a different amount of money to pay for each edifice" in {
+    val country = CountryCard("서울", 1000)
 
-    assert(country.totalTollFee === 300000)
+    assert(country.totalTollFee === 3000)
+
+    assert(country.edificeUpgrade(2000))
+
+    assert(country.totalTollFee === 6000)
+
+    assert(country.edificeUpgrade(4000))
+
+    assert(country.totalTollFee === 12000)
+
+    assert(country.edificeUpgrade(8000))
+
+    assert(country.totalTollFee === 24000)
   }
 
   "totalValueOfEdifice" should "return total value of edifice" in {
