233bab74a33130e9ee19b6be24b27e7adc633665
sungjine456 <sungjine456@gmail.com>
2018-04-09 14:27:48 +0900

    match 문을 통해서 땅을 살 수 있을 때와 살 수 없을 때로 구분

--- booroomarble/src/main/scala/oop/scala/domain/map/CountryPiece.scala
+++ booroomarble/src/main/scala/oop/scala/domain/map/CountryPiece.scala
@@ -10,18 +10,17 @@ case class CountryPiece(card: CountryCard) extends Piece {
   // TODO: 방법1. event와 비슷한 유형을 만들고 event를 반환하도록 하여 시스템에서 처리하도록 한다?
   override def visit(user: User): Unit = {
     owner match {
-      case None =>
+      case None if user.maxMoney >= card.edificeUpgradePrice =>
         // TODO: 사용자가 땅을 사기를 원할 때 땅을 살 수 있도록 해야한다.
-        if(user.maxMoney >= card.edificeUpgradePrice) {
-          user.addCard(card)
-          owner = Some(user)
-        }
+        user.addCard(card)
+        owner = Some(user)
       case Some(ownerUser) if ownerUser == user => // TODO: 사용자가 건물 짓기를 원한다면 건물을 지을 수 있도록 해야한다.
         // 산다고 했을 때
         if(ownerUser.maxMoney >= card.edificeUpgradePrice){
           card.edificeUpgrade(ownerUser.maxMoney)
         }
       case Some(_) => owner.get.receive(user.payMoney(MoneyBundle(card.totalTollFee)))
+      case None =>
     }
   }
 }
